568a421fd2766a05de3201e38fd8c7f9cf17b581
SmartVar <130911742+SmartVar@users.noreply.github.com>
2025-05-08 21:53:57 +0530

    changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-import SocialAuthForm from "@/components/forms/SocialAuthForm";
+// import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
@@ -9,7 +9,7 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
       <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
         <div className="flex items-center justify-between gap-2">
           <div className="space-y-2.5">
-            <h1 className="h2-bold text-dark100_light900">Join PostalZone</h1>
+            <h1 className="h2-bold text-dark100_light900">Join Postal-Zone</h1>
             <p className="paragraph-regular text-dark500_light400">
               To get minor civil/electrical work solutions for your postal
               buildings
@@ -26,7 +26,7 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
 
         {children}
 
-        <SocialAuthForm />
+        {/* <SocialAuthForm /> */}
       </section>
     </main>
   );
